eab00024b1b43a3547a848cbcdb0b6560a43d4f8
mncnf <mncnf0113@gmail.com>
2025-05-24 04:03:33 +0900

    add: button

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+// エントリーポイント
 void main() {
+  // MyAppを起動
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // buildメソッドは、ウィジェットツリーを構築するためのメソッド
+  // ChangeNotifierProviderは、状態管理のためのプロバイダを作成する
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -25,20 +29,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// アプリの状態を管理するクラス
+// ChangeNotifierを継承しているので、状態が変化したときに通知することができる
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners(); // 状態が変化したことを通知
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // watchメソッドを使って、MyAppStateの状態を監視する
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext(); // ボタンが押されたときにgetNextメソッドを呼び出す
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
